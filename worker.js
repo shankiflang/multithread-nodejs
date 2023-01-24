5d01e9c4ce0881ab0b2e891de8b4289e30dad09d
Benjamin BENOIT <shankiflang@gmail.com>
2023-01-24 15:34:39 +0100

    Update

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 const { workerData, parentPort } = require("worker_threads");
 
 let counter = 0;
-for (let i = 0; i < 200_000_000_000 / workerData.thread_count; i++) {
+for (let i = 0; i < 20_000_000_000 / workerData.thread_count; i++) {
   counter++;
 }
 
